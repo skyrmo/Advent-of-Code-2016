dd09d344690a86965c327033d7823bd4447f34bd
Skyrmo <owaingskyrme@gmail.com>
2025-12-26 22:30:51 +0800

    Completed Day 8, parts 1 & 2.

--- day_08/day_08_part_2.py
+++ day_08/day_08_part_2.py
@@ -1,15 +1,14 @@
-
 import os
-import collections
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
         # 2. Read as a list of lines
-        # return data.split('\n')
+        return data.split("\n")
 
         # 3. Read as a list of integers
         # return [int(line) for line in data.split('\n')]
@@ -19,16 +18,48 @@ def parse_input(file_path):
 
         return data
 
+
 def solve(input_data):
-    print(input_data)
+    h, w = 6, 50
+
+    grid = [[0] * w for _ in range(h)]
+
+    for line in input_data:
+        if line.startswith("rect"):
+            _, size = line.split()
+            rec_w, rec_h = [int(x) for x in size.split("x")]
+            # print(line, rec_w, rec_h)
+
+            for r in range(int(rec_h)):
+                for c in range(int(rec_w)):
+                    grid[r][c] = 1
+
+        elif line.startswith("rotate row"):
+            _, _, row, _, shift = line.split(" ")
+            row = int(row[2:])
+            shift = int(shift)
+            grid[row] = grid[row][-shift:] + grid[row][:-shift]
+
+        elif line.startswith("rotate column"):
+            _, _, col, _, shift = line.split()
+            col = int(col[2:])
+            shift = int(shift)
+            old_row = [grid[r][col] for r in range(h)]
+            new_row = old_row[-shift:] + old_row[:-shift]
+            for r in range(h):
+                grid[r][col] = new_row[r]
+
+    for row in grid:
+        print("".join(["#" if x == 1 else " " for x in row]))
+
 
 def main():
     # Get the directory of the current script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the input file path relative to the script's location
-    # input_path = os.path.join(script_dir, 'input.txt')
-    input_path = os.path.join(script_dir, 'sample_input.txt')
+    input_path = os.path.join(script_dir, "input.txt")
+    # input_path = os.path.join(script_dir, "sample_input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -37,5 +68,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 08, Part Two: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
