4ef47cca760b82a4fe5a4e67253a7f5435b712d4
Skyrmo <owaingskyrme@gmail.com>
2026-01-03 01:45:42 +0800

    Completed Day 13, parts 1 & 2.

--- day_13/day_13_part_2.py
+++ day_13/day_13_part_2.py
@@ -1,10 +1,10 @@
-
-import os
 import collections
+import os
+
 
 def parse_input(file_path):
     # Parse the input file
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Read the entire file
         data = file.read().strip()
 
@@ -19,8 +19,57 @@ def parse_input(file_path):
 
         return data
 
+
 def solve(input_data):
-    print(input_data)
+    num = 1362
+    tgt = (39, 31)
+    h, w = 40, 32
+
+    grid = [["."] * w for _ in range(h)]
+
+    for r in range(h):
+        for c in range(w):
+            grid[r][c] = (
+                "."
+                if sum(
+                    [
+                        int(x)
+                        for x in bin(
+                            (c * c) + (3 * c) + (2 * c * r) + r + (r * r) + num
+                        )[2:]
+                    ]
+                )
+                % 2
+                == 0
+                else "#"
+            )
+
+    for row in grid:
+        print(row)
+    print("\n")
+
+    q = collections.deque([(1, 1, 0)])
+    grid[1][1] = 0
+
+    while q:
+        r, c, steps = q.popleft()
+
+        for nr, nc in ((r - 1, c), (r, c + 1), (r + 1, c), (r, c - 1)):
+            if 0 <= nr < h and 0 <= nc < w and grid[nr][nc] == ".":
+                q.append((nr, nc, steps + 1))
+                grid[nr][nc] = steps + 1
+
+    for row in grid:
+        print(row)
+
+    result = 0
+    for r in range(h):
+        for c in range(w):
+            if grid[r][c] not in [".", "#"] and grid[r][c] <= 50:
+                result += 1
+
+    return result
+
 
 def main():
     # Get the directory of the current script
@@ -28,7 +77,7 @@ def main():
 
     # Construct the input file path relative to the script's location
     # input_path = os.path.join(script_dir, 'input.txt')
-    input_path = os.path.join(script_dir, 'sample_input.txt')
+    input_path = os.path.join(script_dir, "sample_input.txt")
 
     # Parse input
     parsed_input = parse_input(input_path)
@@ -37,5 +86,6 @@ def main():
     result = solve(parsed_input)
     print(f"Solution for Day 13, Part Two: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
